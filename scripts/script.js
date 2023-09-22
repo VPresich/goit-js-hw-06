0aedfec654bf8a55bd29c520341ee5872feceb05
VPresich <valya.alexandrova.presich@gmail.com>
2023-09-22 19:13:14 +0200

    Добавлены скрипты по JS - 6.

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let username;
+console.log(typeof username); // "undefined"
+
+let inputValue = null;
+console.log(typeof inputValue); // "object"
+
+const quantity = 17;
+console.log(typeof quantity); // "number"
+
+const isSidebarOpen = false;
+console.log(typeof isSidebarOpen); // "boolean"
+
+const message = "JavaScript is awesome!";
+console.log(message); // JavaScript is awesome!
